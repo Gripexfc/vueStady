cc1dd024b36368dc99720a1ae0ecfb1f274b7d36
fanchen <2837903280@qq.com>
2022-01-30 14:07:56 +0800

    watcher dep  发布订阅模式

--- vue/src/vue/vue.js
+++ vue/src/vue/vue.js
@@ -1,5 +1,6 @@
 
 function poxy(vm) {
+    //代理
     Object.keys(vm.$data).forEach(key=>{
         Object.defineProperty(vm,key,{
             get() {
@@ -14,24 +15,25 @@ function poxy(vm) {
 }
 
 function defineReactive(obj,key,val) {
+    const dep = new Dep();
     //递归处理响应
     obServe(val);
     Object.defineProperty(obj, key, {
         get() {
-            console.log('get')
+            Dep.target && dep.depAdd(Dep.target)
             return val;
         },
         set(v) {
-            console.log('set')
             if (v === val) return;
-            // upData()
+            dep.notify();
             return val = v;
         }
     })
 }
 
 function obServe(obj) {
-    if (typeof val !== 'object' && obj == null) {
+    console.log(typeof obj,'typeof obj')
+    if (typeof obj !== 'object' || obj == null) {
         return obj;
     }
 
@@ -51,13 +53,14 @@ class Vue{
         console.log(options,'options')
         obServe(this.$data);
         poxy(this);
-        new Compile(this.$options.el);
+        new Compile(this.$options.el,this);
     }
 }
 
 class Compile {
-    constructor(el) {
+    constructor(el, vm) {
         this.$el = document.querySelector(el);
+        this.$vm = vm;
         this.compile(this.$el);
     }
 
@@ -70,9 +73,29 @@ class Compile {
                     this.compile(node);
                 }
             } else if(this.isInter(node)) {
-
+                this.compileText(node);
             }
         })
+    }
+
+    updata(node, exp, dir) {
+        //找到对应更新函数
+        const fn = this[dir + 'Updata'];
+        fn(node, this.$vm[exp]);
+
+        new Watcher(this.$vm, exp, function(val) {
+            fn && fn(node, val)
+        })
+    }
+
+    textUpdata(node, exp) {
+        // v-text
+        node.textContent = exp;
+    }
+
+    htmlUpdata(node, exp) {
+        // v-html
+        node.innerHTML = exp;
     }
 
     isElement(node) {
@@ -89,36 +112,56 @@ class Compile {
             const attrName = attr.name;
             const exp = attr.value;
             if (this.isDir(attrName)) {
-
+                const dir = attrName.substring(2);
+                this.updata(node, exp, dir);
+                console.log(attrName.substring(2),'attrName.subString(2);')
             }
         })
+    }
+
+    compileText(node) {
+        // {{}}
+        this.updata(node, RegExp.$1, 'text')
     }
 
     isDir(name) {
         console.log(name.startsWith('v-'),'name.startsWith()')
         return name.startsWith('v-');
     }
-
-
 }
 
 
 
 
 class Watcher {
+    constructor(vm, key, watcher) {
+        this.watcher = watcher;
+        this.key = key;
+        this.$vm = vm;
 
+        // debugger
+        Dep.target = this;
+        this.$vm[this.key];
+        Dep.target = null;
+    }
+
+    updata() {
+        this.watcher.call(this.vm, this.$vm[this.key]);
+    }
 }
 
 class Dep {
     constructor() {
-
+        this.dep = [];
     }
 
-    depAdd() {
-
+    depAdd(target) {
+        this.dep.push(target);
     }
     notify() {
-
+        this.dep.forEach(watcher => {
+            watcher.updata();
+        })
     }
 }
 
