3660188351c984f8b4e69249418db7970564029c
fanchen <fanchen@jmgo.com>
2022-02-09 19:17:27 +0800

    vue源码解析

---
+++ vue-2.6/src/core/instance/index.js
@@ -0,0 +1,25 @@
+import { initMixin } from './init'
+import { stateMixin } from './state'
+import { renderMixin } from './render'
+import { eventsMixin } from './events'
+import { lifecycleMixin } from './lifecycle'
+import { warn } from '../util/index'
+
+function Vue (options) {
+  if (process.env.NODE_ENV !== 'production' &&
+    !(this instanceof Vue)
+  ) {
+    warn('Vue is a constructor and should be called with the `new` keyword')
+  }
+  //初始化生命周期、事件、render vm上的属性  $parant,$children等，
+  // 在berofcreated -初始化props,methods,data,computed,watch,- created
+  this._init(options)
+}
+
+initMixin(Vue)
+stateMixin(Vue)
+eventsMixin(Vue)
+lifecycleMixin(Vue)
+renderMixin(Vue)
+
+export default Vue
